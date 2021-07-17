9053c3e1c482b1b346796c5bef0f71ffee8fdd8a
rashad101 <rah.rony@gmail.com>
2021-07-17 18:13:07 +0200

    fixed typos

--- modules.py
+++ modules.py
@@ -42,8 +42,8 @@ MODEL_CONFIG_CLASSES = list(MODEL_WITH_LM_HEAD_MAPPING.keys())
 MODEL_TYPES = tuple(conf.model_type for conf in MODEL_CONFIG_CLASSES)
 
 
-def train(args, train_dataset, model: PreTrainedModel, tokenizer: PreTrainedTokenizer) -> Tuple[int,float]:
-
+def train(args, train_dataset, model: PreTrainedModel, tokenizer: PreTrainedTokenizer) -> Tuple[int, float]:
+    """ Train the model """
     if args.local_rank in [-1, 0]:
         tb_writer = SummaryWriter()
 
@@ -56,7 +56,7 @@ def train(args, train_dataset, model: PreTrainedModel, tokenizer: PreTrainedToke
 
     train_sampler = RandomSampler(train_dataset) if args.local_rank == -1 else DistributedSampler(train_dataset)
     train_dataloader = DataLoader(
-        train_dataset, sampler=train_sampler, batch_size=args.train_batch_size, collate_fn=collate, drop_last = True
+        train_dataset, sampler=train_sampler, batch_size=args.train_batch_size, collate_fn=collate, drop_last=True
     )
 
     if args.max_steps > 0:
@@ -68,7 +68,6 @@ def train(args, train_dataset, model: PreTrainedModel, tokenizer: PreTrainedToke
     model = model.module if hasattr(model, "module") else model  # Take care of distributed/parallel training
     model.resize_token_embeddings(len(tokenizer))
     # add_special_tokens_(model, tokenizer)
-
 
     # Prepare optimizer and schedule (linear warmup and decay)
     no_decay = ["bias", "LayerNorm.weight"]
@@ -84,26 +83,29 @@ def train(args, train_dataset, model: PreTrainedModel, tokenizer: PreTrainedToke
         optimizer, num_warmup_steps=args.warmup_steps, num_training_steps=t_total
     )
 
-
+    # Check if saved optimizer or scheduler states exist
     if (
             args.model_name_or_path
             and os.path.isfile(os.path.join(args.model_name_or_path, "optimizer.pt"))
             and os.path.isfile(os.path.join(args.model_name_or_path, "scheduler.pt"))
     ):
-        optimizer.load_state_dict(torch.load(os.path.join(args.model_name_or_path, "optimier.pt")))
+        # Load in optimizer and scheduler states
+        optimizer.load_state_dict(torch.load(os.path.join(args.model_name_or_path, "optimizer.pt")))
         scheduler.load_state_dict(torch.load(os.path.join(args.model_name_or_path, "scheduler.pt")))
 
     if args.fp16:
         try:
             from apex import amp
         except ImportError:
-            raise ImportError("Please install apex from https://www.github.com/nvidia/apex to use fp16 training")
-        model, optimizer = amp.initialize(model, optimizer, opt_level = args.fp16_opt_level)
+            raise ImportError("Please install apex from https://www.github.com/nvidia/apex to use fp16 training.")
+        model, optimizer = amp.initialize(model, optimizer, opt_level=args.fp16_opt_level)
 
+    # multi-gpu training (should be after apex fp16 initialization)
     if args.n_gpu > 1:
         model = torch.nn.DataParallel(model)
 
-    if args.local_rank!=-1:
+    # Distributed training (should be after apex fp16 initialization)
+    if args.local_rank != -1:
         model = torch.nn.parallel.DistributedDataParallel(
             model, device_ids=[args.local_rank], output_device=args.local_rank, find_unused_parameters=True
         )
@@ -118,22 +120,21 @@ def train(args, train_dataset, model: PreTrainedModel, tokenizer: PreTrainedToke
         args.train_batch_size
         * args.gradient_accumulation_steps
         * (torch.distributed.get_world_size() if args.local_rank != -1 else 1),
-        )
+    )
     logger.info("  Gradient Accumulation steps = %d", args.gradient_accumulation_steps)
     logger.info("  Total optimization steps = %d", t_total)
-
 
     global_step = 0
     epochs_trained = 0
     steps_trained_in_current_epoch = 0
-
+    # Check if continuing training from a checkpoint
     if args.model_name_or_path and os.path.exists(args.model_name_or_path):
         try:
+            # set global_step to gobal_step of last saved checkpoint from model path
             checkpoint_suffix = args.model_name_or_path.split("-")[-1].split("/")[0]
             global_step = int(checkpoint_suffix)
             epochs_trained = global_step // (len(train_dataloader) // args.gradient_accumulation_steps)
-            steps_trained_in_current_epoch = global_step % (
-                    len(train_dataloader) // args.gradient_accumulation_steps)
+            steps_trained_in_current_epoch = global_step % (len(train_dataloader) // args.gradient_accumulation_steps)
 
             logger.info("  Continuing training from checkpoint, will skip to saved global_step")
             logger.info("  Continuing training from epoch %d", epochs_trained)
@@ -142,34 +143,34 @@ def train(args, train_dataset, model: PreTrainedModel, tokenizer: PreTrainedToke
         except ValueError:
             logger.info("  Starting fine-tuning.")
 
+    tr_loss, logging_loss = 0.0, 0.0
 
-    tr_loss, loggin_loss = 0.0, 0.0
     model.zero_grad()
     train_iterator = trange(
-        epochs_trained , int(args.num_train_epochs), desc="Epoch", disable=args.local_rank not in [-1,0]
+        epochs_trained, int(args.num_train_epochs), desc="Epoch", disable=args.local_rank not in [-1, 0]
     )
-    set_seed(args)
-
+    set_seed(args)  # Added here for reproducibility
     for _ in train_iterator:
-        epochs_iterator = tqdm(train_dataloader, desc="Iteration", disable=args.local_rank not in [-1,0])
-        for step, batch in enumerate(epochs_iterator):
+        epoch_iterator = tqdm(train_dataloader, desc="Iteration", disable=args.local_rank not in [-1, 0])
+        for step, batch in enumerate(epoch_iterator):
 
-            if steps_trained_in_current_epoch>0:
-                steps_trained_in_current_epoch -=1
+            # Skip past any already trained steps if resuming training
+            if steps_trained_in_current_epoch > 0:
+                steps_trained_in_current_epoch -= 1
                 continue
 
             inputs, labels = (batch, batch)
             if inputs.shape[1] > 1024: continue
             inputs = inputs.to(args.device)
-            labels = inputs.to(args.device)
+            labels = labels.to(args.device)
             model.train()
             outputs = model(inputs, labels=labels)
-            loss = outputs[0]
+            loss = outputs[0]  # model outputs are always tuple in transformers (see doc)
 
-            if args.n_gpu> 1:
-                loss = loss.mean()
-            if args.gradient_accumulation_steps>1:
-                loss = loss/args.gradient_accumulation_steps
+            if args.n_gpu > 1:
+                loss = loss.mean()  # mean() to average on multi-gpu parallel training
+            if args.gradient_accumulation_steps > 1:
+                loss = loss / args.gradient_accumulation_steps
 
             if args.fp16:
                 with amp.scale_loss(loss, optimizer) as scaled_loss:
@@ -177,34 +178,39 @@ def train(args, train_dataset, model: PreTrainedModel, tokenizer: PreTrainedToke
             else:
                 loss.backward()
 
-
-            tr_loss+=loss.item()
-            if (step+1) % args.gradient_accumulation_steps == 0:
+            tr_loss += loss.item()
+            if (step + 1) % args.gradient_accumulation_steps == 0:
                 if args.fp16:
-                    torch.nn.utils.clip_grad_norm(amp.master_params(optimizer), args.max_grad_norm)
+                    torch.nn.utils.clip_grad_norm_(amp.master_params(optimizer), args.max_grad_norm)
                 else:
-                    torch.nn.utils.clip_grad_norm(model.parameters(), args.max_grad_norm)
-
+                    torch.nn.utils.clip_grad_norm_(model.parameters(), args.max_grad_norm)
                 optimizer.step()
-                scheduler.step()
+                scheduler.step()  # Update learning rate schedule
                 model.zero_grad()
-                global_step+=1
+                global_step += 1
 
-                if args.local_rank in [-1,0] and args.logging_steps > 0 and global_step% args.logging_steps==0:
-                    if (args.local_rank==-1 and args.evaluate_during_training):
+                if args.local_rank in [-1, 0] and args.logging_steps > 0 and global_step % args.logging_steps == 0:
+                    # Log metrics
+                    if (
+                            args.local_rank == -1 and args.evaluate_during_training
+                    ):  # Only evaluate when single GPU otherwise metrics may not average well
                         results = evaluate(args, model, tokenizer)
                         for key, value in results.items():
-                            tb_writer.add_scalar("eval{}".format(key), value, global_step)
+                            tb_writer.add_scalar("eval_{}".format(key), value, global_step)
                     tb_writer.add_scalar("lr", scheduler.get_lr()[0], global_step)
-                    tb_writer.add_scalar("loss", (tr_loss-loggin_loss)/ args.logging_steps, global_step)
-                    loggin_loss+=tr_loss
+                    tb_writer.add_scalar("loss", (tr_loss - logging_loss) / args.logging_steps, global_step)
+                    logging_loss = tr_loss
 
-                if args.local_rank in [-1,0] and args.save_steps > 0 and global_step % args.save_steps == 0:
+                if args.local_rank in [-1, 0] and args.save_steps > 0 and global_step % args.save_steps == 0:
                     checkpoint_prefix = "checkpoint"
+                    # Save model checkpoint
                     output_dir = os.path.join(args.output_dir, "{}-{}".format(checkpoint_prefix, global_step))
                     os.makedirs(output_dir, exist_ok=True)
-                    model_to_save = (model.module if hasattr(model, "module") else model)
+                    model_to_save = (
+                        model.module if hasattr(model, "module") else model
+                    )  # Take care of distributed/parallel training
                     model_to_save.save_pretrained(output_dir)
+                    tokenizer.save_pretrained(output_dir)
 
                     torch.save(args, os.path.join(output_dir, "training_args.bin"))
                     logger.info("Saving model checkpoint to %s", output_dir)
@@ -215,13 +221,14 @@ def train(args, train_dataset, model: PreTrainedModel, tokenizer: PreTrainedToke
                     torch.save(scheduler.state_dict(), os.path.join(output_dir, "scheduler.pt"))
                     logger.info("Saving optimizer and scheduler states to %s", output_dir)
 
-            if args.max_steps> 0 and global_step>args.max_steps:
-                epochs_iterator.close()
+            if args.max_steps > 0 and global_step > args.max_steps:
+                epoch_iterator.close()
                 break
         if args.max_steps > 0 and global_step > args.max_steps:
             train_iterator.close()
             break
-    if args.local_rank in [-1,0]:
+
+    if args.local_rank in [-1, 0]:
         tb_writer.close()
 
     return global_step, tr_loss / global_step
